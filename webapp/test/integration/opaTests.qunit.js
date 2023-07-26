95dd5a7e609d3628c27797526d1f3622e6b11040
Federico <fjaimes@exaccs.com>
2023-07-26 16:48:44 -0300

    TP

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Exaccs/trabajopractico/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Exaccs/trabajopractico/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
